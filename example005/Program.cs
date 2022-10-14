84aa5a4e0159a0cd4c4fb41cb6bea8689a606a8d
«Creektus» <5347100@gmail.com.com  git config --global user.name Creektusgit config --global user.email 5347100@gmail.com>
2022-10-14 21:11:17 +0300

    add example005 Hello Maria

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("write username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Yup! It's Maria! WooHoo");
+}
+else
+{
+    Console.Write("Hello! ");
+    Console.WriteLine(username);
+}
